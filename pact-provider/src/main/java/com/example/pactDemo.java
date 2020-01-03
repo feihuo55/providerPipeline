279f1a4ee28923cd68c85f619aeea5d3bf21f25d
Guo Lin <lguo1@statestreet.com>
2020-01-03 14:41:37 +0800

    initial provider pipeline

---
+++ pact-provider/src/main/java/com/example/pactDemo.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class pactDemo {
+
+    private int value;
+
+    public pactDemo(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
